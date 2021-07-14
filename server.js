51b986edf8ae36c5e68edf5a6934cabe5e62ed92
JK <saikitjk@gmail.com>
2021-07-13 19:42:24 -0700

    commented out unused code

--- server.js
+++ server.js
@@ -23,38 +23,38 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/api/screenshot", async (req, res) => {
-  const { url } = req.body;
+// app.post("/api/screenshot", async (req, res) => {
+//   const { url } = req.body;
 
-  try {
-    let screenshot = await takeScreenshot(url);
-    let img = screenshot.toString("base64");
-    res.send({ result: img });
-  } catch (e) {
-    // catch errors and send error status
-    console.log(e);
-    res.sendStatus(500);
-  }
-});
+//   try {
+//     let screenshot = await takeScreenshot(url);
+//     let img = screenshot.toString("base64");
+//     res.send({ result: img });
+//   } catch (e) {
+//     // catch errors and send error status
+//     console.log(e);
+//     res.sendStatus(500);
+//   }
+// });
 
-async function takeScreenshot(url) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle0" });
-  // await page.setViewport({
-  //   width: 1400,
-  //   height: 1000,
-  //   deviceScaleFactor: 1,
-  // });
-  await page.setViewport({ width: 1200, height: 1200 });
-  const screenshot = await page.screenshot({ fullPage: true });
+// async function takeScreenshot(url) {
+//   const browser = await puppeteer.launch({
+//     headless: true,
+//     args: ["--no-sandbox"],
+//   });
+//   const page = await browser.newPage();
+//   await page.goto(url, { waitUntil: "networkidle0" });
+//   // await page.setViewport({
+//   //   width: 1400,
+//   //   height: 1000,
+//   //   deviceScaleFactor: 1,
+//   // });
+//   await page.setViewport({ width: 1200, height: 1200 });
+//   const screenshot = await page.screenshot({ fullPage: true });
 
-  await browser.close();
-  return screenshot;
-}
+//   await browser.close();
+//   return screenshot;
+// }
 
 app.post("/api/savescreenshot", async (req, res) => {
   const { url } = req.body;
@@ -126,7 +126,7 @@ async function saveScreenshot(url, sessID, count) {
     args: ["--no-sandbox"],
   });
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle0" });
+  await page.goto(url, { waitUntil: "networkidle0", timeout: 0 });
   // await page.setViewport({
   //   width: 1400,
   //   height: 1000,
