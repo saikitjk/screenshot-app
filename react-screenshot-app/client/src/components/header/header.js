4a379d478602a0ae082875d49b1237f19d9a2a7a
JK <saikitjk@gmail.com>
2021-09-03 16:47:11 -0700

    created react components

---
+++ react-screenshot-app/client/src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function header() {
+  return (
+    <div className="jumbotron">
+      <h1>Screenshot App</h1>
+      <h3>Capture screenshot of any URL</h3>
+      <hr></hr>
+    </div>
+  );
+}
