5e8d54e409e36b45611085ed56856d28919b5c97
JK <saikitjk@gmail.com>
2021-09-03 21:06:06 -0700

    added the href link in the html file to enable boostrap

--- react-screenshot-app/client/src/components/header/header.js
+++ react-screenshot-app/client/src/components/header/header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Jumbotron } from "reactstrap";
 
 export default function header() {
   return (
-    <div className="jumbotron">
-      <h1>Screenshot App</h1>
-      <h3>Capture screenshot of any URL</h3>
-      <hr></hr>
+    <div>
+      <Jumbotron>
+        <h1>Screenshot App</h1>
+        <h3>Capture screenshot of any URL</h3>
+        <hr></hr>
+      </Jumbotron>
     </div>
   );
 }
