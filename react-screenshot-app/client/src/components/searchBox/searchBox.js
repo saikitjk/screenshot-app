b097c531db64188276f15d9949b23cebda9ca2ad
JK <saikitjk@gmail.com>
2021-09-06 23:22:35 -0700

    added react button for 4 functionlities

--- react-screenshot-app/client/src/components/searchBox/searchBox.js
+++ react-screenshot-app/client/src/components/searchBox/searchBox.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FormGroup, Input, Button } from "reactstrap";
-import { FaSyncAlt } from "react-icons/fa";
+import { FaSyncAlt, FaCameraRetro, FaDownload } from "react-icons/fa";
 import "./searchBox.css";
 
 export default function searchBox() {
@@ -16,33 +16,53 @@ export default function searchBox() {
       </FormGroup>
 
       <div className="text-right">
-        <Button className="btn btn-primary btn-md" type="submit" id="reset">
+        <Button color="primary" className="resetBtn" type="submit" id="reset">
           <FaSyncAlt />
           Reset
         </Button>
 
-        <button type="submit" class="btn btn-primary btn-md" id="reset">
+        <Button color="primary" className="saveBtn" type="submit" id="save-btn">
+          <FaCameraRetro />
+          Grab screenshots
+        </Button>
+
+        <Button
+          color="primary"
+          className="loadBtn"
+          type="button"
+          id="load-btn"
+          disabled
+        >
+          Processing. Please wait...
+        </Button>
+
+        <Button color="primary" className="dlBtn" type="submit" id="download">
+          <FaDownload />
+          Download File
+        </Button>
+
+        {/* <button type="submit" class="btn btn-primary btn-md" id="reset">
           <span class="fa fa-refresh"></span>
           Reset
-        </button>
+        </button> */}
 
-        <button type="submit" class="btn btn-primary btn-md" id="save-btn">
+        {/* <button type="submit" class="btn btn-primary btn-md" id="save-btn">
           <span class="fa fa-camera"></span> Grab screenshots
-        </button>
-        <button class="btn btn-primary" type="button" id="load-btn" disabled>
+        </button> */}
+        {/* <button class="btn btn-primary" type="button" id="load-btn" disabled>
           <span
             class="spinner-border spinner-border-sm"
             role="status"
             aria-hidden="true"
           ></span>
           Processing. Please wait...
-        </button>
+        </button> */}
 
-        <a class="download" href="/api/download" target="_blank">
+        {/* <a class="download" href="/api/download" target="_blank">
           <button type="submit" class="btn btn-primary btn-md" id="download">
             <span class="fa fa-camera"></span>Download File
           </button>
-        </a>
+        </a> */}
       </div>
     </div>
   );
