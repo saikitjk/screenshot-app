c05cb3c2d990abeafc08834b28a08fa385c6b361
JK <saikitjk@gmail.com>
2021-09-11 23:17:12 -0700

    added sessID generator

--- react-screenshot-app/client/src/components/main/main.js
+++ react-screenshot-app/client/src/components/main/main.js
@@ -18,10 +18,21 @@ class Main extends React.Component {
 
   handleGrab = (event) => {
     event.preventDefault();
+    ///generate random sessID
+    let ranGen = () => {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    };
+
+    var sessID = ranGen();
+    console.log(sessID);
+    ///generate random sessID
+
     if (this.state.urlArray.length < 1) {
       alert("Please enter an URL");
     }
-    console.log("click");
+    console.log("Clicked. SessID is :" + sessID);
     console.log(this.state);
     console.log("type: " + typeof this.state.urlArray);
   };
